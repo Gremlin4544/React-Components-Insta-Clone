923eee54330c62a4bbf6ca569d3955901f911cc9
Gremlin4544 <alve@mckethen.com>
2020-01-21 12:57:07 -0500

    added state for data on app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
@@ -16,12 +16,15 @@ import dummyData from "./dummy-data";
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+
+  const [data, setData] = useState(0);
+
   return (
     <div className="App">
       {/* Add components here  and pass props where appropriate */}
       <SearchBar />
-      <PostsPage />
-      <dummyData />
+      <PostsPage posts="post" />
+      <dummyData data="data" />
     </div>
   );
 };
